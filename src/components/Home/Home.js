8b6e42bee3425679f4aaeee49ddb63c2f5e97bb5
Priyom Saha <priyom1499@gmail.com>
2020-06-29 10:02:51 +0530

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './Home.css'
 import Header from '../Header/Header'
 import Background from './Background'
-import Login from './Login'
+//import Login from './Login'
 
 function Home() {
     return (
