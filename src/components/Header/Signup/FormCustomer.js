dc3cf7ca2d3ae549c826aa75df290b8606e60891
Priyom Saha <priyom1499@gmail.com>
2020-07-01 11:32:57 +0530

    Location icon fixed

--- src/components/Header/Signup/FormCustomer.js
+++ src/components/Header/Signup/FormCustomer.js
@@ -1,9 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, InputGroup } from 'react-bootstrap'
 
 function FormCustomer() {
+    const axios = require("axios");
 
-//password Toogle
+    //Track Location
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+
+    const [city, setCity] = useState("");
+
+    const getLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+        });
+    }
+
+    useEffect(() => {
+        axios({
+            "method": "GET",
+            "url": "https://geocodeapi.p.rapidapi.com/GetNearestCities",
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "geocodeapi.p.rapidapi.com",
+                "x-rapidapi-key": "0577cace1cmsh16549506bfab3dep1a7116jsnaf23580d2668",
+                "useQueryString": true
+            }, "params": {
+                "latitude": latitude,
+                "longitude": longitude,
+                "range": "0"
+            }
+        })
+            .then((response) => {
+                if (latitude === 0 || longitude === 0)
+                    setCity("");
+                else
+                    setCity(response.data[0].City)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        console.log(latitude, longitude);
+
+    }, [latitude,longitude])
+
+    //password Toogle
     const [passwordType, setPasswordType] = useState('password');
     const [passwordToggleText, setPasswordToggleText] = useState('Show');
     const passwordToggle = () => {
@@ -17,44 +59,6 @@ function FormCustomer() {
         }
     }
 
-//Track Location
-
-        const [city,setCity] = useState("") ;
-    const axios = require("axios");
-        const [latitude,setLatitude] = useState(0) ;
-        const [longitude,setLongitude] = useState(0) ;
-        const getLocation = ()=>{
-            navigator.geolocation.getCurrentPosition((position)=>{
-                var lat = position.coords.latitude;
-                var long = position.coords.longitude;
-                setLatitude(lat);
-                setLongitude(long);
-                });
-
-              (function(){
-                axios({
-                    "method":"GET",
-                    "url":"https://geocodeapi.p.rapidapi.com/GetNearestCities",
-                    "headers":{
-                    "content-type":"application/octet-stream",
-                    "x-rapidapi-host":"geocodeapi.p.rapidapi.com",
-                    "x-rapidapi-key":"0577cace1cmsh16549506bfab3dep1a7116jsnaf23580d2668",
-                    "useQueryString":true
-                    },"params":{
-                    "latitude":latitude,
-                    "longitude":longitude,
-                    "range":"0"
-                    }
-                    })
-                    .then((response)=>{
-                        setCity(response.data[0].City)
-                    })
-                    .catch((error)=>{
-                      console.log(error)
-                    })
-              })();
-
-        }
     return (
         <Form>
             <Form.Group controlId="formGridName">
@@ -102,9 +106,9 @@ function FormCustomer() {
                     <Form.Label>City</Form.Label>
                     <Form.Control placeholder="Click The Icon To locate" value={city} />
                     <span className="location" onClick={getLocation}>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                    </svg>
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        </svg>
                     </span>
                 </Form.Group>
 
@@ -113,9 +117,6 @@ function FormCustomer() {
                     <Form.Control type="number" placeholder="Enter Pin-code" />
                 </Form.Group>
             </Form.Row>
-            {latitude}
-            <br/>
-            {longitude}
         </Form>
     )
 }
